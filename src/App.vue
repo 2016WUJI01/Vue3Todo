7aaf4474a082020bcd4695decdf2a9d274b1f6d6
吴嘉顺 <870435170@qq.com>
2022-03-19 21:51:56 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Home from './components/Home.vue';
+// This starter template is using Vue 3 <script setup> SFCs
+</script>
+
+<template>
+  <Home></Home>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
