e6958243999157d41f68f0a77b9d70b34bce7ca9
吴嘉顺 <870435170@qq.com>
2022-03-20 15:01:43 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,68 +1,80 @@
 <template>
-  <input type="text" placeholder="添加 todo" v-model="newTodoTitle" @keydown.enter="addTodo" />
-  <button @click="addTodo">添加</button>
-  <button @click="clearAllDone">清理已完成</button>
-  <div v-if="todos.length">
-    <div v-for="(todo, index) in todos" :key="todo.title">
-      <input type="checkbox" v-model="todo.done" />
-      <span>{{ todo.title }}</span>
-      <span @click="deleteTodo(index)">❎</span>
+  <input
+    type="text"
+    placeholder="添加 todo"
+    v-model="newTodoValue"
+    @keydown.enter="createtodo(newTodoValue)"
+  />
+  <button @click="createtodo(newTodoValue)">添加</button>
+  <div v-if="todoList.length">
+    <div v-for="todo in todoList" :key="todo.id">
+      <input type="checkbox" v-model="todo.completed" />
+      {{ todo.value }}
+      <span @click="removetodo(todo)">❎</span>
     </div>
-    <div>
-      <span>全选</span>
-      <input type="checkbox" v-model="selectAll" />
-      <span>{{ hasDone }}/{{ todos.length }}</span>
-    </div>
+    <div>{{ remaining }}/{{ todoList.length }}</div>
   </div>
   <div v-else>暂无数据</div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 //定义变量
-interface Todo {
-  title: string
-  done: boolean
+interface todo {
+  id: number
+  value: string
+  completed?: boolean
 }
-const newTodoTitle = ref<string>('')
-const todos = ref<Todo[]>([
-  { title: '吃饭', done: true },
-  { title: '睡觉', done: false }
-])
-const hasDone = computed(() => todos.value.filter(item => item.done).length)  // 已经做了的事项列表
+interface filters {
+  [key: string]: (todo: todo[]) => todo[]
+}
+const newTodoValue = ref<string>('')
 
-//添加todo
-const addTodo = () => {
-  if (newTodoTitle.value) {
-    todos.value.push({
-      title: newTodoTitle.value,
-      done: false
-    })
-    newTodoTitle.value = ''
-  }
-}
-//删除todo
-const deleteTodo = (index: number) => {
-  todos.value.splice(index, 1)
-}
-
-//清除已完成
-const clearAllDone = () => {
-  todos.value = todos.value.filter(item => !item.done)
-}
-
-//选择全部
-const selectAll = computed<boolean>({
-  get() {
-    return hasDone.value === todos.value.length
+const STORAGE_KEY = 'vue3todo'
+const todoStorage = {
+  fetch() {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    return todos
   },
-  set(value: boolean) {
-    todos.value.forEach(item => {
-      item.done = value
-    })
+  save(todos: todo[]) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
+}
+const todoList = ref<todo[]>(todoStorage.fetch())
+const visibility = ref<string>('all')
+
+const filters: filters = {
+  all(todos) {
+    return todos
+  },
+  active(todos) {
+    return todos.filter((todo) => !todo.completed)
+  },
+  computed(todos) {
+    return todos.filter((todo) => todo.completed)
+  },
+}
+const remaining = computed(() => filters.active(todoList.value).length)
+
+watch(() => todoList.value, (todos) => {
+  todoStorage.save(todos)
+}, {
+  deep: true
 })
 
+const createtodo = (value: string) => {
+  if (value) {
+    todoList.value.push({
+      id: new Date().getTime(),
+      value: value,
+      completed: false
+    })
+  }
+}
+
+const removetodo = (todo: todo) => {
+  todoList.value = todoList.value.filter(item => item.id !== todo.id)
+}
 
 </script>
 
